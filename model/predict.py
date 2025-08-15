bfefb9459e55ab85461731a1f02a909e0e047ccd
muhamed-aroui <muhamed.aroui@gmail.com>
2025-08-15 12:02:55 -0400

    minor changes

--- model/predict.py
+++ model/predict.py
@@ -1,5 +1,11 @@
 import pickle
+import pandas as pd
 
 #load the ML model
 with open("model.plk",'rb') as f:
     model = pickle.load(f)
+
+def predict_output(input :dict):
+    df= pd.DataFrame([input])
+    ...
+
--- schema/user_input.py
+++ schema/user_input.py
@@ -6,15 +6,16 @@ class InsuranceInput(BaseModel):
     Pydantic model to define the input data structure for insurance premium prediction.
     """
     age: Annotated[int, Field(..., gt=0, description="Age of the primary beneficiary.")]
-    sex: Annotated[Literal['Male','Female'],Field(..., description="Gender of the beneficiary (male or female).")]
     height: Annotated[float, Field(...,gt=0 ,description="Height of the beneficiary")]
     weight: Annotated[float, Field(...,gt=0,lt=3.5, description="Weight of the beneficiary")]
     income_USD: Annotated[float,Field(...,gt=0,description="Annual salary of the beneficiary")]
-    children: Annotated[int, Field(..., ge=0, description="Number of children covered by the insurance.")]
     smoker: Annotated [bool, Field(..., description="Smoker status (True or False).")]
-    region: Annotated[str, Field(..., description="The beneficiary's residential region.")]
+    city: Annotated[str, Field(..., description="The beneficiary's residential region.")]
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
+       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the beneficiary')]
 
-    @field_validator('region')
+
+    @field_validator('city')
     @classmethod
     def format_region(cls, region:str) -> str:
         region = region.split().title()

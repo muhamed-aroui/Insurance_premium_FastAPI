5922a49f40cf8e33adec33cffa4192955ee9d0c9
muhamed-aroui <muhamed.aroui@gmail.com>
2025-08-18 12:09:56 -0400

    try catch

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def predict_premium(data: InsuranceInput):
         'income_lpa': data.income_lpa,
         'occupation': data.occupation
     }
-    prediction = predict_output(user_input)
-    return JSONResponse(status_code=200, content={'predicted_premium':prediction})
+    try:
+        prediction = predict_output(user_input)
+        return JSONResponse(status_code=200, content={'predicted_premium':prediction})
+    except Exception as e:
+        return JSONResponse(status_code=500,content=str(e))
